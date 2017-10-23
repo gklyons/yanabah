32645c09fe3b7992f4462942840ca87026fc6a40
gklyons <30358106+gklyons@users.noreply.github.com>
2017-10-23 15:37:44 -0600

    Created Tea Model, ShopTVC and storyboard

---
+++ Yanabah/Yanabah/Models/Tea.swift
@@ -0,0 +1,9 @@
+//
+//  Tea.swift
+//  Yanabah
+//
+//  Created by Garrett Lyons on 10/19/17.
+//  Copyright © 2017 Garrett Lyons. All rights reserved.
+//
+
+import Foundation
---
+++ Yanabah/Yanabah/Shop/ShopTeaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopTeaTableViewCell.swift
+//  Yanabah
+//
+//  Created by Garrett Lyons on 10/23/17.
+//  Copyright © 2017 Garrett Lyons. All rights reserved.
+//
+
+import UIKit
+
+class ShopTeaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
